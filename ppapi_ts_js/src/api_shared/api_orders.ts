afd8818da9f770595f0ef9693b93c358b48a0ef7
Marcos <marcos.cruz.batista@gmail.com>
2019-07-10 16:27:36 -0300

    add filter to get latest agents

--- ppapi_ts_js/src/api_shared/api_orders.ts
+++ ppapi_ts_js/src/api_shared/api_orders.ts
@@ -257,8 +257,61 @@ export class APIOrders extends APIBaseChild {
         });
     });
   }
-  public getLatestActiveCoachAgents() {
-    const uri = `/orders/coach/last-agents`;
+  public getLatestActiveCoachAgents(agentId?: number,page?: number | string,sortBy?: SortBy,search?: Search,filters?: Filters) {
+    let uri = `/orders/coach/last-agents`;
+    if (page && page > 0) {
+      uri = uri + `?p=${page}`;
+    }
+    if (sortBy) {
+      uri = uri + `&s=${sortBy.key}&a=${sortBy.asc === '1' ? 1 : 0}`;
+    }
+
+    if (search) {
+      uri = uri + `&ctx=${search.col}&q=${search.val}`;
+    }
+
+    if (filters) {
+      if (filters.value.min !== '') {
+        uri = uri + `&min=${filters.value.min}`;
+      }
+      if (filters.value.max !== '') {
+        uri = uri + `&max=${filters.value.max}`;
+      }
+      if (filters.client_login) {
+        uri = uri + `&client_login=${filters.client_login}`;
+      }
+      if (filters.recurrent_client) {
+        uri = uri + `&recurrent_client=${filters.recurrent_client}`;
+      }
+      if (filters.scheduled_calls) {
+        uri = uri + `&scheduled_calls=${filters.scheduled_calls}`;
+      }
+      if (filters.calls !== '') {
+        uri = uri + `&calls=${filters.calls}`;
+      }
+      if (filters.created_date !== '') {
+        uri = uri + `&created_date=${filters.created_date}`;
+      }
+      if (filters.expired_slip_bank) {
+        uri = uri + `&expired_bank_slip=${filters.expired_slip_bank}`;
+      }
+      if (filters.payment_method !== '') {
+        uri = uri + `&payment_method=${filters.payment_method}`;
+      }
+      if (filters.payment_delay !== '') {
+        uri = uri + `&payment_delay=${filters.payment_delay}`;
+      }
+      if (filters.expired_medication !== '') {
+        uri = uri + `&expired_medication=${filters.expired_medication}`;
+      }
+      if (filters.medication_out_of_stock !== '') {
+        uri = uri + `&medication_out_of_stock=${filters.medication_out_of_stock}`;
+      }
+    }
+
+    if (agentId) {
+      uri = uri + `&agent_id=${agentId}`;
+    }
     return new Promise<APIResponse<CoachAgentStat[]>>(resolve => {
       this.getJSON(uri)
         .catch(error => {
@@ -322,7 +375,13 @@ export class APIOrders extends APIBaseChild {
         });
     });
   }
-  public getNoPaymentsOrders(agentId?: number, page?: number | string, sortBy?: SortBy, search?: Search, filters?: Filters) {
+  public getNoPaymentsOrders(
+    agentId?: number,
+    page?: number | string,
+    sortBy?: SortBy,
+    search?: Search,
+    filters?: Filters,
+  ) {
     let uri = `/orders/no-payment`;
     if (page && page > 0) {
       uri = uri + `?p=${page}`;
@@ -518,7 +577,7 @@ export class APIOrders extends APIBaseChild {
     });
   }
 
-  public getContinuousUseOrdersAgents(page?: number | string,sortBy?: SortBy,search?: Search,filters?: Filters) {
+  public getContinuousUseOrdersAgents(page?: number | string, sortBy?: SortBy, search?: Search, filters?: Filters) {
     let uri = `/orders/continuous-use/agents`;
     if (page && page > 0) {
       uri = uri + `?p=${page}`;
@@ -572,7 +631,7 @@ export class APIOrders extends APIBaseChild {
     });
   }
 
-  public getRepurchase(agentId?: number,page?: number | string, sortBy?: SortBy, search?: Search, filters?: Filters) {
+  public getRepurchase(agentId?: number, page?: number | string, sortBy?: SortBy, search?: Search, filters?: Filters) {
     let uri = `/orders/repurchase`;
     if (page && page > 0) {
       uri = uri + `?p=${page}`;
@@ -608,7 +667,6 @@ export class APIOrders extends APIBaseChild {
         uri = uri + `&repurchase_date=${filters.repurchase_date}`;
       }
     }
-
 
     if (agentId) {
       uri = uri + `&agent_id=${agentId}`;
