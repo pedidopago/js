0b5733e4b4c2520bfae5398d0d968847a7bb2c2d
Gabriel Ochsenhofer <gabriel@pedidopago.com.br>
2019-07-26 13:49:37 -0300

    fix via lint

--- ppapi_ts_js/src/api_agent/index.ts
+++ ppapi_ts_js/src/api_agent/index.ts
@@ -2,15 +2,12 @@ import { APIBase } from '../api_shared/api_base';
 import util from '../util';
 import { APICoaches } from './coaches';
 export class API extends APIBase {
-  public coaches: APICoaches;
-
-  public static newFromCookie(
-    baseurl: string = '',
-    cookiename: string = '',
-  ) {
+  public static newFromCookie(baseurl: string = '', cookiename: string = '') {
     const jwt = util.getCookie(cookiename);
     return new API(baseurl, jwt);
   }
+
+  public coaches: APICoaches;
 
   constructor(baseurl: string = '', jwt: string = '') {
     super(baseurl, jwt, '/v1/agt', true);
